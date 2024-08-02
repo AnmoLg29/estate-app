e072afbe606670d0094663d697b4e8db969c95fb
Anmol Gupta <anmolgupta290402@gmail.com>
2024-08-02 23:34:42 +0530

    added development url

--- socket/app.js
+++ socket/app.js
@@ -3,7 +3,7 @@ import { Server } from "socket.io";
 const port = process.env.PORT || 4000
 const io = new Server({
     cors: {
-        origin: "http://localhost:5174",
+        origin: "http://localhost:5173",
     },
 });
 
