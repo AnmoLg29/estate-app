f2de4f23d7c545f9dfdd8b273a87fe12aa6e447b
Anmol Gupta <anmolgupta290402@gmail.com>
2024-08-03 15:36:26 +0530

    added frontend deploy url

--- socket/app.js
+++ socket/app.js
@@ -3,7 +3,7 @@ import { Server } from "socket.io";
 const port = process.env.PORT || 4000
 const io = new Server({
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://interestate.vercel.app",
     },
 });
 
